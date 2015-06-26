9d2b46d8d9307a9f7edc6a25fa791367e87091da
anmej <snysmymrik@walla.co.il>
2015-06-26 16:26:30 +0300

    first commit

---
+++ src/gamestate.rs
@@ -0,0 +1,169 @@
+extern crate rand;
+use rand::{Rng, SeedableRng, XorShiftRng};
+
+pub const GRID_HEIGHT: i16 = 900;
+pub const GRID_WIDTH: i16 = 1440;
+
+pub const BLOCK_SIZE: i16 = 1;
+
+pub const WINDOW_HEIGHT: i16 = GRID_HEIGHT * BLOCK_SIZE;
+pub const WINDOW_WIDTH: i16 = GRID_WIDTH * BLOCK_SIZE;
+
+pub const RAIN_SPARSENESS: i16 = 5;
+
+pub const X: bool = true;
+pub const o: bool = false;
+
+#[derive(PartialEq, Clone)]
+pub struct Loc {
+    pub x: i16,
+    pub y: i16
+}
+
+//pub struct Neighbours ((bool,bool,bool),(bool,bool),(bool,bool,bool));
+
+pub struct Map {pub map: Box<[[bool; GRID_HEIGHT as usize]; GRID_WIDTH as usize]>}
+
+pub struct GameState {
+    pub particles: Vec<Loc>,
+    pub obstacles: Vec<Loc>,
+    pub indexes_to_remove: Vec<usize>,
+    pub map: Map, //2D array to check occupancy
+    pub max_x: i16,
+    pub max_y: i16,
+    pub rng: XorShiftRng
+}
+
+impl Map {
+    fn is_occupied(&self, x: i16, y: i16) -> bool {
+        // not valid is not occupied
+        if !GameState::is_valid(x, y) {false} else {self.map[x as usize][y as usize]}
+    }
+
+    fn get_neighbours(&mut self, x: i16, y: i16) -> [bool; 8] {
+        [self.is_occupied(x-1, y-1), self.is_occupied(x, y-1), self.is_occupied(x+1, y-1),
+        self.is_occupied(x-1, y), self.is_occupied(x+1, y),
+        self.is_occupied(x-1, y+1), self.is_occupied(x, y+1), self.is_occupied(x-1, y+1)]
+
+    }
+
+    fn add_coord_map(&mut self, x: i16, y: i16) {
+        self.map[x as usize][y as usize] = true;
+    }
+
+    fn remove_coord_map(&mut self, x: i16, y: i16) {
+        self.map[x as usize][y as usize] = false;
+    }
+}
+
+impl GameState {
+    //check if point in inside the grid
+    pub fn is_valid(x: i16, y: i16) -> bool {
+        if x<0 || x>=GRID_WIDTH || y<0 || y>=GRID_HEIGHT {false} else {true}
+    }
+
+    pub fn update(&mut self) {
+        //MAIN LOGIC
+        for (index, particle) in self.particles.iter_mut().enumerate() {
+            let (x, y) = (particle.x, particle.y);
+            let neighbours = self.map.get_neighbours(x, y);
+            let (x_new, y_new) = match neighbours {
+               /*above  side below*/
+                [_,_,_, _,_, _,o,_] => ( x, y+1 ),
+                [_,X,_, o,X, _,X,_] => ( x-1, y ),
+                [_,X,_, X,o, _,X,_] => ( x+1, y ),
+                [_,X,_, o,o, _,X,_] => ( if y&2 == 0 {x-1} else {x+1}, y ), //deterministic choice
+                //[_,X,_, o,o, _,X,_] => ( if self.rng.gen::<i16>()&2 == 0 {x-1} else {x+1}, y ), //random choice
+                                        _ => continue,
+            };
+            if GameState::is_valid(x_new, y_new) {
+                self.map.remove_coord_map(x, y);
+                self.map.add_coord_map(x_new, y_new);
+                *particle = Loc {x: x_new, y: y_new};
+            } else {
+                self.map.map[x as usize][y as usize] = false;
+                self.indexes_to_remove.push(index);
+            }
+        }
+        //must use .rev(), otherwise you can't remove the last element
+        for index in self.indexes_to_remove.iter().rev() {
+            self.particles.swap_remove(*index);
+        }
+        self.indexes_to_remove.clear();
+    }//end update
+
+    pub fn rain(&mut self) {
+        for x in 1*(GRID_WIDTH/20)..19*(GRID_WIDTH/20) {
+            if !self.map.is_occupied(x, 0) && self.rng.gen::<i16>()&RAIN_SPARSENESS == 0 {
+                self.particles.push(Loc {x: x, y: 0});
+                self.map.add_coord_map(x, 0);
+            }
+        }
+    }
+
+    fn remove_particle_list (&mut self, x: i16, y: i16) {
+        let loc = Loc {x: x, y: y};
+        self.particles.retain(|p|{ p != &loc});
+    }
+
+    fn remove_obstacle_list (&mut self, x: i16, y: i16) {
+        let loc = Loc {x: x, y: y};
+        self.obstacles.retain(|p|{ p != &loc});
+    }
+
+    fn add_particle (&mut self, x: i16, y: i16) {
+        self.map.add_coord_map(x, y);
+        let loc = Loc {x: x, y: y};
+        self.particles.push(loc);
+    }
+
+    fn add_obstacle (&mut self, x: i16, y: i16) {
+        self.map.add_coord_map(x, y);
+        let loc = Loc {x: x, y: y};
+        self.obstacles.push(loc);
+    }
+
+    fn remove_coord(&mut self, x: i16, y: i16) {
+        self.map.remove_coord_map(x, y);
+        self.remove_obstacle_list(x, y);
+        self.remove_particle_list(x, y);
+    }
+
+    pub fn remove_square (&mut self, ux: i16, uy: i16, dx: i16, dy: i16) {
+        if Self::is_valid(ux + dx, uy + dy) {
+            for x in ux..ux+dx {
+                for y in uy..uy+dy {
+                    if self.map.is_occupied(x, y) {
+                     self.remove_coord(x, y);
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn paint_square_obstacles (&mut self, ux: i16, uy: i16, dx: i16, dy: i16) {
+        if Self::is_valid(ux + dx, uy + dy) {
+            for x in ux..ux+dx {
+                for y in uy..uy+dy {
+                    if !self.map.is_occupied(x, y) {
+                        self.map.add_coord_map(x, y);
+                        self.add_obstacle(x, y);
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn new() -> GameState {
+        GameState {
+                particles: Vec::with_capacity(10_000),
+                obstacles: Vec::with_capacity(10_000),
+                indexes_to_remove: Vec::with_capacity(1000),
+                map: Map { map: box [[false; GRID_HEIGHT as usize]; GRID_WIDTH as usize] },
+                max_x: GRID_WIDTH,
+                max_y: GRID_HEIGHT,
+                rng: SeedableRng::from_seed([1, 2, 3, 4])
+            }
+    }
+
+}//end impl GameState
---
+++ src/main.rs
@@ -0,0 +1,124 @@
+#![allow(non_upper_case_globals, dead_code)]
+#![feature(box_syntax)]
+#![feature(slice_patterns)]
+
+mod gamestate;
+use gamestate::*;
+
+extern crate rand;
+//use rand::{Rng, SeedableRng, XorShiftRng};
+
+extern crate piston;
+extern crate image;
+extern crate graphics;
+extern crate sdl2_window;
+extern crate opengl_graphics;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use opengl_graphics::{ GlGraphics, OpenGL, Texture };
+use sdl2_window::Sdl2Window;
+use image::GenericImage;
+use piston::input::{ MouseButton };
+use piston::window::{WindowSettings, AdvancedWindow};
+use piston::event::*;
+
+fn main() {
+    let mut game = GameState::new();
+
+    let mut paused = true;
+    let mut raining = true;
+
+    let mut mouse_buttons_pressed = (false, false); //left, right
+
+    let opengl = OpenGL::_3_2;
+    let (width, height) = (WINDOW_WIDTH as u32, WINDOW_HEIGHT as u32);
+    let window = Sdl2Window::new(
+        opengl,
+        WindowSettings::new("Falling Sand", (width, height))
+        .exit_on_esc(true)
+        //what those do?
+        .vsync(true)
+        .samples(4)
+        .fullscreen(true)
+    );
+    let window = Rc::new(RefCell::new(window));
+    let mut image = image::ImageBuffer::from_pixel(width, height, image::Rgba([0, 0, 0, 255]));
+    let mut texture = Texture::from_image(&image);
+    let ref mut gl = GlGraphics::new(opengl);
+    for e in window.clone().events().max_fps(120).ups(60) {
+        use piston::event::*;
+        use piston::input::{Button, Key};
+
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            match key {
+                Key::Space => {
+                    paused = !paused;
+                    window.borrow_mut().set_capture_cursor(!paused); //remove cursor when game is running
+                },
+                Key::R => {raining = !raining},
+                _ => {}
+            }
+        }
+
+        if let Some(_) = e.update_args() {
+            //try put everything here?
+            //Update game and image on every loop
+            if !paused {
+                if raining {game.rain()}
+                game.update();
+            }//if !paused {
+        }
+
+        if paused {
+            if let Some(button) = e.press_args() {
+                match button {
+                    Button::Mouse(MouseButton::Left) => {mouse_buttons_pressed.0 = true},
+                    Button::Mouse(MouseButton::Right) => {mouse_buttons_pressed.1 = true},
+                    _ => {}
+                }
+            }
+            if let Some(button) = e.release_args() {
+                match button {
+                    Button::Mouse(MouseButton::Left) => {mouse_buttons_pressed.0 = false},
+                    Button::Mouse(MouseButton::Right) => {mouse_buttons_pressed.1 = false},
+                    _ => {}
+                }
+            }
+            if let Some(pos) = e.mouse_cursor_args() {
+                //get mouse coordinates and draw something if there is place
+                let (mouse_x, mouse_y) = (pos[0] as i16, pos[1] as i16);
+
+                match mouse_buttons_pressed {
+                   (true, false) => {
+                       game.paint_square_obstacles(mouse_x, mouse_y, 30, 30);
+                   }
+                   (false, true) => {
+                       //remove everything from map and both lists
+                       game.remove_square(mouse_x, mouse_y, 30, 30);
+                   }
+                   _ => {}
+               }
+            }
+        } //if paused
+
+        if let Some(args) = e.render_args() {
+
+            image = image::ImageBuffer::from_pixel(width, height, image::Rgba([0, 0, 0, 255]));
+            for particle in game.particles.iter() {
+                image.put_pixel(particle.x as u32, particle.y as u32, image::Rgba([238,232,170,255]));
+            }
+            for obstacle in game.obstacles.iter() {
+                image.put_pixel(obstacle.x as u32, obstacle.y as u32, image::Rgba([128,0,0,255]));
+            }
+            texture.update(&image);
+            gl.draw(args.viewport(), |c, gl| {
+
+                graphics::clear([1.0; 4], gl);
+                graphics::image(&texture, c.transform, gl);
+            });
+
+        };
+    }
+}
